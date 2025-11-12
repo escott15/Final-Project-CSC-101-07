fcd5bdea74e1175f7431ffeb067441af51a5fde3
Sean Ellwein <sdellwei@calpoly.edu>
2025-11-11 19:07:42 -0800

    added function to compare value to threshold

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 ###
 
-# Given an input float and a threshold float, return True if the input is larger
-# than the threshold and False otherwise
-def over_threshold(value: float, threshold: float) -> bool:
+# Given an input float and a threshold float, return True if input is larger than threshold, return False otherwise
+def over_threshold(value:float,threshold:float)->float:
     if value > threshold:
         return True
     else:
