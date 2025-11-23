f69016f9d8b627ba0df89fff91e8dfa3d77df5c7
Sean Ellwein <sdellwei@calpoly.edu>
2025-11-23 15:21:02 -0800

    Added comments to explain code

--- household_energy_consumption.py
+++ household_energy_consumption.py
@@ -1,17 +1,28 @@
 from data import *
 
-
+# This function is specifically designed to read the 'household_energy_consumption.txt' file and convert
+# all the lines of text into a list of Household class objects
+# Input: None (Although the 'household_energy_consumption.txt' file is required to run anything
+# Output: list[Household]
 def create_households()->list[Household]:
+    # Open and read the 'household_energy_consumption.txt' file, then use .readline() function to
+    # create a list that can be operated on
     main_data = open('household_energy_consumption.txt', 'r')
     data_list = main_data.readlines()
     main_data.close()
 
+    # This segment modifies the elements of data_list so that each string is stripped of new lines and
+    # turned into a list where each element is a data point for a household on a given day. Append that
+    # list to data_set
     data_set = []
     for string in data_list:
         string = string.strip("\n")
         new_list = string.split(',')
         data_set.append(new_list)
 
+    # In this segment, for every list in data_set, gather all the data for an individual household over the
+    # course of a week and store each data point into its own categorical list. Once the loop encounters a
+    # new household, create a household object with the lists and repeat.
     i_d = data_set[0][0]
     dates = []
     energy_consumption_kwh = []
